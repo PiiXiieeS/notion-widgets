1b233122765040b19884b3e63bb0f2a1ee12f2fb
Jose Gascon <PiiXiieeS@users.noreply.github.com>
2026-01-26 01:30:53 +0100

    Add weather widget script with theme support

---
+++ weather/script.js
@@ -0,0 +1,44 @@
+function script(d, s, id) {
+	var js,
+		fjs = d.getElementsByTagName(s)[0];
+	js = d.createElement(s);
+	js.id = id;
+	js.src = 'https://weatherwidget.io/js/widget.min.js';
+	fjs.parentNode.insertBefore(js, fjs);
+}
+
+// DYNAMIC THEME SETTINGS BASED ON OS PREFERENCE
+
+function light() {
+	document.documentElement.setAttribute('data-theme', 'pure');
+	document.getElementById('weather').setAttribute('data-theme', 'pure');
+	document.getElementById('weather').removeAttribute('data-basecolor');
+	document.getElementById('weather').setAttribute('data-textcolor', '#37352f');
+	document.getElementById('weather').removeAttribute('data-cloudfill');
+	document.getElementById('weather').setAttribute('data-suncolor', '#F58f70');
+	script(document, 'script', 'weatherwidget-io-js');
+}
+
+function dark() {
+	document.documentElement.setAttribute('data-theme', 'gray');
+	document.getElementById('weather').setAttribute('data-theme', 'gray');
+	document.getElementById('weather').setAttribute('data-basecolor', '#191919');
+	document.getElementById('weather').removeAttribute('data-textcolor');
+	document.getElementById('weather').setAttribute('data-cloudfill', '#191919');
+	document.getElementById('weather').setAttribute('data-suncolor', '#F58f70');
+	script(document, 'script', 'weatherwidget-io-js');
+}
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	dark();
+} else {
+	light();
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+	if (event.matches) {
+		dark();
+	} else {
+		light();
+	}
+});
