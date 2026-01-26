d290bbaaa81052071d45ec0029c37103ea3474d9
Jose Gascon <PiiXiieeS@users.noreply.github.com>
2026-01-26 02:38:24 +0100

    Remove weather widget script loading function
    Removed the script function that dynamically loads the weather widget.

--- weather/script.js
+++ weather/script.js
@@ -1,12 +1,3 @@
-function script(d, s, id) {
-	var js,
-		fjs = d.getElementsByTagName(s)[0];
-	js = d.createElement(s);
-	js.id = id;
-	js.src = 'https://weatherwidget.io/js/widget.min.js';
-	fjs.parentNode.insertBefore(js, fjs);
-}
-
 // DYNAMIC THEME SETTINGS BASED ON OS PREFERENCE
 
 function light() {
